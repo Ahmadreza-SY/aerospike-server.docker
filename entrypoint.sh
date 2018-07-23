59eb8577934b3fcd02cafde0bae870da0182d687
Ahmadreza-SY <ahmadreza.saboor2012@gmail.com>
2018-07-23 18:09:24 +0430

    update aerospike, whole config to one env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 set -e
 
-export CORES=$(grep -c ^processor /proc/cpuinfo)
-export SERVICE_THREADS=${SERVICE_THREADS:-$CORES}
-export TRANSACTION_QUEUES=${TRANSACTION_QUEUES:-$CORES}
-export TRANSACTION_THREADS_PER_QUEUE=${TRANSACTION_THREADS_PER_QUEUE:-4}
-export LOGFILE=${LOGFILE:-/dev/null}
-export SERVICE_ADDRESS=${SERVICE_ADDRESS:-any}
-export SERVICE_PORT=${SERVICE_PORT:-3000}
-export HB_ADDRESS=${HB_ADDRESS:-any}
-export HB_PORT=${HB_PORT:-3002}
-export FABRIC_ADDRESS=${FABRIC_ADDRESS:-any}
-export FABRIC_PORT=${FABRIC_PORT:-3001}
-export INFO_ADDRESS=${INFO_ADDRESS:-any}
-export INFO_PORT=${INFO_PORT:-3003}
-export NAMESPACE=${NAMESPACE:-test}
-export REPL_FACTOR=${REPL_FACTOR:-2}
-export MEM_GB=${MEM_GB:-1}
-export DEFAULT_TTL=${DEFAULT_TTL:-30d}
-export STORAGE_GB=${STORAGE_GB:-4}
-export SERVICE_ACCESS_ADDRESS=${SERVICE_ACCESS_ADDRESS:-192.168.1.100}
-export MESH_SEED_ADDERSS_PORT_LIST=${MESH_SEED_ADDERSS_PORT_LIST}
+export AEROSPIKE_CONFIG_ALL=${AEROSPIKE_CONFIG_ALL}
 
 # Fill out conffile with above values
 if [ -f /etc/aerospike/aerospike.template.conf ]; then
